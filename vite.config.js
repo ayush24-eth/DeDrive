e7dc54c71f8bcdd3d0ed51646012f604e524186c
ayush24-eth <ayushtmsl611@gmail.com>
2023-04-23 16:35:47 +0530

    update vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: 'window',
+    // global: 'window',
     'process.env': {}
   },
+  build: {
+    rollupOptions: {
+      external: [
+        /^node:.*/,
+      ]
+    }
+  }
 })
